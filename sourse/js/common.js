f4a0e662cc9d6190fdc2c995b1cc04dfe1bb31d6
Pavel <wol1414@gmail.com>
2021-05-07 03:55:02 +0300

    done animate

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -54,7 +54,7 @@ const JSCCommon = {
 				type: 'inline',
 				arrows: false,
 				infobar: false,
-				touch: false, 
+				touch: false,
 				autoFocus: false,
 				i18n: {
 					en: {
@@ -69,7 +69,7 @@ const JSCCommon = {
 				afterClose: function () {
 					root.style.setProperty('--spacing-end', null);
 				},
-				
+
 			});
 		})
 		// $(link).fancybox({
@@ -123,7 +123,7 @@ const JSCCommon = {
 		if (menu.classList.contains("active")) {
 			this.btnToggleMenuMobile.forEach(element => element.classList.remove("on"));
 			this.menuMobile.classList.remove("active");
-			[document.body, document.querySelector('html')].forEach(el => el.classList.remove("fixed")); 
+			[document.body, document.querySelector('html')].forEach(el => el.classList.remove("fixed"));
 		}
 
 	},
@@ -280,7 +280,7 @@ function eventHandler() {
 		lazy: {
 			loadPrevNext: true,
 		},
-		watchOverflow: true, 
+		watchOverflow: true,
 		loop: true,
 	}
 
@@ -322,13 +322,13 @@ function eventHandler() {
 		//centeredSlides: true,
 
 		breakpoints: {
-			0:{
+			0: {
 				spaceBetween: 16,
 			},
-			1200:{
+			1200: {
 				spaceBetween: 20,
 			},
-			1400:{
+			1400: {
 				spaceBetween: 24,
 			},
 		},
@@ -353,7 +353,7 @@ function eventHandler() {
 	let caruselSlider = new Swiper('.carusel__slider--not-center-js', {
 		...defaultSl,
 		slidesPerView: 'auto',
-		loopedSlides: 6, 
+		loopedSlides: 6,
 		loop: false,
 		navigation: {
 			nextEl: '.swiper-button-next',
@@ -367,10 +367,10 @@ function eventHandler() {
 		loop: true,
 
 		breakpoints: {
-			0:{
+			0: {
 				spaceBetween: 16,
 			},
-			992:{
+			992: {
 				spaceBetween: 0,
 			},
 		},
@@ -394,14 +394,14 @@ function eventHandler() {
 	});
 
 	//yandex lazy
-	window.setTimeout(function (){
+	window.setTimeout(function () {
 		let yandexScript = document.createElement('script');
 		yandexScript.setAttribute('src', 'https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;apikey=ef0b1dde-1d01-4d5b-9636-c00e2adbee98');
 		yandexScript.setAttribute('type', 'text/javascript');
 
 		document.body.appendChild(yandexScript);
 		console.log(yandexScript);
-		window.setTimeout(function (){
+		window.setTimeout(function () {
 			ymaps.ready(function () {
 				var myMap = new ymaps.Map('map', {
 					center: [44.95308614480347, 34.109081034312545],
@@ -440,8 +440,8 @@ function eventHandler() {
 
 				// Создаем кнопку, с помощью которой пользователи смогут менять местами начальную и конечную точки маршрута.
 				var switchPointsButton = new ymaps.control.Button({
-					data: {content: "Поменять местами", title: "Поменять точки местами"},
-					options: {selectOnClick: false, maxWidth: 160}
+					data: { content: "Поменять местами", title: "Поменять точки местами" },
+					options: { selectOnClick: false, maxWidth: 160 }
 				});
 				// Объявляем обработчик для кнопки.
 				switchPointsButton.events.add('click', function () {
@@ -459,18 +459,18 @@ function eventHandler() {
 	//end luckyone js
 
 
- 
-		var wow = new WOW({
-			mobile: false,
-			animateClass: 'animate__animated', 
-		});
-		
-		$('body').removeClass("op"); 
-		wow.init();
-		setTimeout(() => {
+
+	var wow = new WOW({
+		mobile: false,
+		animateClass: 'animate__animated',
+	});
+
+	$('body').removeClass("op");
+	wow.init();
+	setTimeout(() => {
 	}, 1000);
 	setTimeout(() => {
-		$('.header').removeClass("op"); 
+		$('.header').removeClass("op");
 	}, 500);
 
 
@@ -478,19 +478,26 @@ function eventHandler() {
 	var controller = new ScrollMagic.Controller();
 
 
-	function animateText(text, parent, y="-=200%") {
-		var tween = TweenMax.to(`${parent} ${text}`, 0.1, { y: y  });
+	function animateText(text, parent, y = "-=200%") {
+		var tween = TweenMax.to(`${parent} ${text}`, 0.1, { y: y });
 		// build scene
 		var scene = new ScrollMagic.Scene({ triggerElement: parent, duration: 1000 })
-		.setTween(tween)
-		// .addIndicators() // add indicators (requires plugin)
-		.addTo(controller);
+			.setTween(tween)
+			// .addIndicators() // add indicators (requires plugin)
+			.addTo(controller);
 	}
-	animateText('#text-id-1', '#block-id-1',"-=50%");
-	animateText('#text-id-2', '#block-id-2',"-=50%");
-	animateText('#text-id-3', '#block-id-3',"-=50%");
+	function animateTextVertical(text, parent, y = "-=200%") {
+		var scene = new ScrollMagic.Scene({ triggerElement: parent, duration: 1000 })
+			.setPin(`${parent} ${text}`)
+			.setTween(tween)
+			// .addIndicators() // add indicators (requires plugin)
+			.addTo(controller);
+	}
+	animateTextVertical('#text-id-1', '#block-id-1', "-=50%");
+	animateTextVertical('#text-id-2', '#block-id-2', "-=50%");
+	animateTextVertical('#text-id-3', '#block-id-3', "-=50%");
+	animateTextVertical('.text', '#sSteps', "-=50%");
 	animateText('.text', '#sRews');
-	animateText('.text', '#sSteps', "-=50%");
 	animateText('.bg-txt', '#sKiteStation');
 	animateText('.bg-txt', '#sLeisure');
 	animateText('.bg-txt', '#sPlace');
@@ -500,8 +507,40 @@ function eventHandler() {
 	animateText('.bg-txt', '#sAccommodation');
 	animateText('.bg-txt', '#sBlog');
 	animateText('.bg-txt', '.footer');
+
+
+
+
+	var tween = TweenMax.to(`.sAbout__row`, 300, { x: '-=50%' });
+	// build scene
+	var scene = new ScrollMagic.Scene({ triggerElement: '.sAbout', duration: 1000 })
+		.setTween(tween)
+		// .addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 	// animateText('.sAdvantages__before', '.sAdvantages');
 
+	var isMobile = (function (a) { return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)); })(navigator.userAgent || navigator.vendor || window.opera);
+
+	// we'd only like to use iScroll for mobile...
+	if (!isMobile) {
+
+		var wipeAnimation = new TimelineMax()
+			// animate to second panel
+
+			.to("#sSteps  .swiper-wrapper", 1, { x: "-100%" })
+
+		// create scene to pin and link animation
+		new ScrollMagic.Scene({
+			triggerElement: "#sSteps ",
+			triggerHook: "onLeave",
+			duration: "400%"
+		})
+			.setPin("#sSteps ")
+			.setTween(wipeAnimation)
+			// .addIndicators() // add indicators (requires plugin)
+			.addTo(controller);
+
+	};
 };
 if (document.readyState !== 'loading') {
 	eventHandler();
